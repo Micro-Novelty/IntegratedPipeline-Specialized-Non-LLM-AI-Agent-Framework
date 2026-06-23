ef9e1b053f2fc03453da7686e9a152f375aba390
Micro-Novelty <hernikpuspita5@gmail.com>
2026-06-23 10:04:16 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,10 @@ extensions = [
 ]
 
 setup(
+    author="Micro-Novelty",
+    version="0.7.3"
+    author_email="hernikpuspita5@gmail.com",
+    license="MIT",
     ext_modules=cythonize(extensions, language_level=3),
     rust_extensions=[
             RustExtension(
@@ -33,5 +37,14 @@ setup(
                 binding="pyo3",
                 optional=True,
             ),
-    ],        
+    ],  
+    python_requires="> 3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
